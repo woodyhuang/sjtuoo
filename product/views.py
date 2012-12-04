94cc53f33f28d637458d6aad51e9779336a5b917
bigtang <107650285@qq.com>
2012-12-04 14:25:52 +0800

    Update product/views.py

--- product/views.py
+++ product/views.py
@@ -3,12 +3,12 @@
 from django import shortcuts
 from django.template import RequestContext
 
-from product.models import ProductCategory
+from product.models import Product
 
 
-def list_category(request):
-    product_categories = ProductCategory.objects.all()
+def list_product(request):
+    product = Product.objects.all()
     
-    return shortcuts.render_to_response('product/category_list.html',
-                            {'product_categories': product_categories},
+    return shortcuts.render_to_response('product/product_list.html',
+                            {'product': product},
                             context_instance=RequestContext(request))
